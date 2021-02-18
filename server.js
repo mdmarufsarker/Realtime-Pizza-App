9f64e7a055e762ad644f27494372eaeb9e52838c
mdmarufsarker <mdmarufsarker2021@gmail.com>
2021-02-19 00:56:51 +0600

    Folder structure and express server setup

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000;
+// const PORT;
+// if(process.env.PORT){
+//     PORT = process.env.PORT
+// }else{
+//     PORT = 3000
+// }
+
+
+app.get('/', (req, res) =>{
+    // Checking output on browser
+    // res.render('Welcome')
+    res.render('home')
+})
+
+// Set Templete Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+// checking port on console
+app.listen(3000, () => {
+    console.log(`Listening on port ${PORT}`);
+})
