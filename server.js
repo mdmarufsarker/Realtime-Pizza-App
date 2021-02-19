989007e224fe40612ffb13ad30483ff5a91b3574
mdmarufsarker <mdmarufsarker2021@gmail.com>
2021-02-20 02:18:24 +0600

    Make cart, login, register page

--- server.js
+++ server.js
@@ -15,16 +15,30 @@ const PORT = process.env.PORT || 3000;
 // Assets
 app.use(express.static('public'))
 
+
+// Set Templete Engine
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+// Routes
 app.get('/', (req, res) =>{
     // Checking output on browser
     // res.render('Welcome')
     res.render('home')
 })
 
-// Set Templete Engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) =>{
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) =>{
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) =>{
+    res.render('auth/register')
+})
 
 // checking port on console
 app.listen(3000, () => {
