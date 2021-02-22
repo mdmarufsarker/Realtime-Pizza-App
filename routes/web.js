b5bc12f31cb43f8a3f1456eecbe42eed69a06039
mdmarufsarker <mdmarufsarker2021@gmail.com>
2021-02-23 03:48:01 +0600

    Auth System

--- routes/web.js
+++ routes/web.js
@@ -1,27 +1,19 @@
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const guest = require('../app/http/middleware/guest')
+
 
 function initRoutes(app){
-    // (req, res) =>{
-    //     // Checking output on browser
-    //     // res.render('Welcome')
-    //     res.render('home')
-    // }
-
-    // app.get('/login', (req, res) =>{
-    //     res.render('auth/login')
-    // })
-    
-    // app.get('/register', (req, res) =>{
-    //     res.render('auth/register')
-    // })
-
     // Routes
     app.get('/', homeController().index)
+    app.get('/login', guest, authController().login)
+    app.post('/login', authController().postLogin)
+    app.get('/register', guest, authController().register)
+    app.post('/register', authController().postRegister)
+    app.post('/logout', authController().logout)
+
     app.get('/cart', cartController().index)
-    app.get('/login', authController().login)
-    app.get('/register', authController().register)
     app.post('/update-cart', cartController().update)
     
 }
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 // Database connection
 const url = 'mongodb://localhost/pizza';
@@ -44,17 +45,28 @@ app.use(session({
     // for secure app 15 min enough
 }))
 
+
+// Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(flash())
 
 // Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
+    
 })
 
 // Set Templete Engine
